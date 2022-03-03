48a83831c07e06ea0cd09b993268d6b3d5497f37
Hannah Malm <hannahemalm616@gmail.com>
2022-03-02 20:05:22 -0600

    commit tree d62647336f93528c09234e78497900689c4e838d

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+//use class because there is a state
+//local state because form state does not need to live in the store
 class Form extends React.Component {
   state = {
     firstName: "John",
     lastName: "Henry"
   }
 
+  //setState should fire every time the form changes.
+  //The event contains data about the target
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
@@ -17,6 +21,20 @@ class Form extends React.Component {
       lastName: event.target.value
     })
   }
+
+
+  //pass in the event of clicking the onSubmit
+  //prevent default form behavor
+  //set the form data using key/value pair  in a hash
+  //sendFormdatasomewhare will eventually be an action using redux
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = {
+      firstName: this.state.firstName, //Here, we are putting together the current form data using the values stored in state.
+      lastName: this.state.lastName,
+    };
+    this.sendFormDataSomewhere(formData);
+  };
 
   render() {
     return (
